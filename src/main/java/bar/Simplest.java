f487429aa28dcccda0e63e247dc022a729a40eae
Fabrice Bellingard <bellingard@gmail.com>
2018-08-09 09:45:51 +0200

    Add some issues

--- src/main/java/bar/Simplest.java
+++ src/main/java/bar/Simplest.java
@@ -2,8 +2,11 @@ package bar;
 
 /**
  * Simple class
+ * TODO
  */
 public class Simplest {
+    
+    public int i;
 
     public static int add(int a, int b) {
     	// introduce a variable that is not needed - just to get a violation
